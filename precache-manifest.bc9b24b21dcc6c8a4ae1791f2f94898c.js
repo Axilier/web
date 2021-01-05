8dd5da2ad47bc1721a59437b57c2b795c27ade75
KLewin23 <afrenchrussian@gmail.com>
2021-01-05 18:05:37 +0000

    Deploying to build from  @ 14915bd0c774f6c820c4d54c32ae936aeedc59cd 🚀

---
+++ precache-manifest.bc9b24b21dcc6c8a4ae1791f2f94898c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cf1378d491f4e3dcc6bf6fe4961683",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbd1555eac0a2bedf6c4",
+    "url": "/static/css/main.19ca675f.chunk.css"
+  },
+  {
+    "revision": "7b7d10bc4b2d302f30ed",
+    "url": "/static/js/2.30b96606.chunk.js"
+  },
+  {
+    "revision": "82c082e276dacd47f333ae753bb5d44b",
+    "url": "/static/js/2.30b96606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd1555eac0a2bedf6c4",
+    "url": "/static/js/main.45a9e303.chunk.js"
+  },
+  {
+    "revision": "9b1f789f4bf6fd968195",
+    "url": "/static/js/runtime-main.f35b5890.js"
+  },
+  {
+    "revision": "2556a4f74e2c523893e6928d6e300f1c",
+    "url": "/static/media/Metropolis-SemiBold.2556a4f7.otf"
+  },
+  {
+    "revision": "d39f016c747fa0b4e85074f9387a7f9b",
+    "url": "/static/media/TextLogo.d39f016c.svg"
+  }
+]);
