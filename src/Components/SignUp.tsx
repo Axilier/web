2951303a1cc80f7e048eac6e11c83441a26c3df5
Kieran Lewin <afrenchrussian@gmail.com>
2021-02-03 00:53:19 +0000

    complete signup I think

--- src/Components/SignUp.tsx
+++ src/Components/SignUp.tsx
@@ -3,13 +3,24 @@ import * as React from "react";
 import { useState } from "react";
 import "../Css/Account.css";
 import "../Css/SignUp.css";
-import { Button, Github, Google, Key, Mail, TextBox, Warning } from "core";
+import {
+    Button,
+    Cross,
+    Github,
+    Google,
+    Key,
+    Mail,
+    Question,
+    TextBox,
+} from "core";
 import { useHistory } from "react-router-dom";
 import TopBar from "./TopBar";
 import StrengthBar from "./StrengthBar";
+import axios from "axios";
 
 const SignUp = () => {
     // eslint-disable-next-line no-unused-vars
+    const [emailValid, setEmailValid] = useState(true);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [reTypedPassword, setReTypedPassword] = useState("");
@@ -47,7 +58,47 @@ const SignUp = () => {
         setPasswordStrength(strength);
     }
 
+    function checkEmail() {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email); // DOC not perfect but will do basic validation, do email validation on server
+    }
+
     /* eslint-disable */
+
+    function passwordCheck(check: boolean): string {
+        return password === "" || reTypedPassword === ""
+            ? "#BEBEBE"
+            : check
+            ? "#1EB932"
+            : "#F01919";
+    }
+
+    function signUp() {
+        if (!checkEmail()) {
+            setEmailValid(false);
+        } else {
+            setEmailValid(true);
+            axios
+                .post(
+                    "http://3.10.226.172/register",
+                    {
+                        email,
+                        password,
+                    },
+                    { withCredentials: true }
+                )
+                .then(
+                    (res) => {
+                        if (res.status === 200) {
+                            history.push("/account");
+                        }
+                    },
+                    () => {
+                        console.log("something went wrong");
+                    }
+                );
+        }
+    }
+
     return (
         <div className={"web-account-main"}>
             <TopBar />
@@ -60,8 +111,24 @@ const SignUp = () => {
                     size={"large"}
                     required
                     label={"Email"}
-                    onChange={(value) => setEmail(value)}
+                    onChange={(value) => {
+                        setEmail(value);
+                    }}
                 />
+                {!emailValid ? (
+                    <div
+                        style={{
+                            color: "#F01919",
+                        }}
+                        className={"web-signup-email-error"}
+                    >
+                        <Cross
+                            iconColor={"#F01919"}
+                            style={{ marginRight: "3px" }}
+                        />
+                        Invalid Email Address
+                    </div>
+                ) : null}
                 <TextBox
                     prefixComponent={<Key />}
                     placeholder={"Enter your password"}
@@ -95,18 +162,84 @@ const SignUp = () => {
                     }}
                 />
 
+                <div
+                    className={"web-signup-strength-notices"}
+                    style={{ color: "#BEBEBE" }}
+                >
+                    <div className={"web-signup-strength-notices-col"}>
+                        <div
+                            style={{
+                                color: passwordCheck(
+                                    password === reTypedPassword
+                                ),
+                            }}
+                        >
+                            <Question
+                                iconColor={passwordCheck(
+                                    password === reTypedPassword
+                                )}
+                            />
+                            Passwords match?
+                        </div>
+                        <div
+                            style={{
+                                color: passwordCheck(password.length >= 8),
+                            }}
+                        >
+                            <Question
+                                iconColor={passwordCheck(password.length >= 8)}
+                            />
+                            At least 8 characters long?
+                        </div>
+                        <div
+                            style={{
+                                color: passwordCheck(/[A-Z]/.test(password)),
+                            }}
+                        >
+                            <Question
+                                iconColor={passwordCheck(
+                                    /[A-Z]/.test(password)
+                                )}
+                            />{" "}
+                            Contains a capital letter?
+                        </div>
+                    </div>
+                    <div className={"web-signup-strength-notices-col"}>
+                        <div
+                            style={{
+                                color: passwordCheck(/[0-9]/.test(password)),
+                            }}
+                        >
+                            <Question
+                                iconColor={passwordCheck(
+                                    /[0-9]/.test(password)
+                                )}
+                            />{" "}
+                            Contains a number?
+                        </div>
+                        <div
+                            style={{
+                                color: passwordCheck(/[^\w\s]/.test(password)),
+                            }}
+                        >
+                            <Question
+                                iconColor={passwordCheck(
+                                    /[^\w\s]/.test(password)
+                                )}
+                            />{" "}
+                            Contains a symbol?
+                        </div>
+                    </div>
+                </div>
+
                 {password === "" && reTypedPassword === "" ? (
                     <div className={"web-signup-strength-text"}>
                         Enter your password above to calculate strength
                     </div>
                 ) : passwordStrength < 4 ? (
                     <div className={"web-signup-strength-text"}>
-                        Your password is too weak please retry{" "}
-                        <Warning
-                            iconColor={"#BEBEBE"}
-                            style={{ height: "13px" }}
-                            className={"web-signup-strength-warning"}
-                        />
+                        Your password is too weak please fix the red issues
+                        above
                     </div>
                 ) : (
                     <div className={"web-signup-strength-text"}>
@@ -119,6 +252,7 @@ const SignUp = () => {
                     style={{ margin: "10px 0" }}
                     label={"sign up"}
                     variant={"contained"}
+                    onClick={() => signUp()}
                 />
                 <div className={"web-account-body-divider"}>
                     <div className={"web-account-body-divider-bar"} />
